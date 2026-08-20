4cf5c12c0acaa43554fc0b6a88cd2c73c617f6a0
Denis Akershteyn <dakerdenis@gmail.com>
2026-08-20 11:15:37 -0700

    Fix linting in urls and health-check

--- chint/config/urls.py
+++ chint/config/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
-from django.views.generic import RedirectView
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 from web.views.health import health_check
 
